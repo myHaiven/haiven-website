56d736b2e723b4cf15f6bf7d9a594417af9a1d3a
Jackie Lu <jlu6fz@gmail.com>
2022-04-02 15:35:10 -0700

    Test branch protection

--- frontend/src/components/Example.tsx
+++ frontend/src/components/Example.tsx
@@ -2,5 +2,5 @@ import { Text } from "@chakra-ui/layout";
 import React from "react";
 
 export default function Example() {
-  return <Text fontSize="6xl">Hello</Text>;
+  return <Text fontSize="5xl">Hello</Text>;
 }
