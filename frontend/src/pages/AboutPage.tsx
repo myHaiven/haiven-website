f4a13e617c2e937a1cd2b96f2f3916b793adae9f
Jackie Lu <jlu6fz@gmail.com>
2022-05-28 23:17:35 -0700

    Add description

--- frontend/src/pages/AboutPage.tsx
+++ frontend/src/pages/AboutPage.tsx
@@ -1,4 +1,4 @@
-import { Container, Heading } from "@chakra-ui/layout";
+import { Container, Heading, Text } from "@chakra-ui/layout";
 import React from "react";
 
 import MainPanel from "../components/MainPanel";
@@ -11,6 +11,17 @@ export default function AboutPage() {
         <Heading fontSize={headingFontSizeBreakpoints} variant="primaryColor">
           About Us
         </Heading>
+        <Text textStyle="greyText">
+          Haiven is a non-profit organization that is devoted to helping victims
+          impacted by intimate partner violence. During our customer outreach,
+          victims expressed a need for more accessible and tailored solutions,
+          and a better understanding of the current resources that exist. Our
+          company operates as a resource hub that focuses on creating our own
+          solutions through building a suite of mobile and web applications that
+          will improve the safety and well-being of victims through accessible
+          and innovative technological solutions while guiding them to resource
+          partners.
+        </Text>
       </MainPanel>
     </Container>
   );
