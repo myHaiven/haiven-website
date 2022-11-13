33b22bfa3a25f3fc80dcf2d14a6c121709b35b30
Jackie Lu <jlu6fz@gmail.com>
2022-11-12 20:14:03 -0800

    Add navbar back to app

--- frontend/pages/_app.tsx
+++ frontend/pages/_app.tsx
@@ -1,9 +1,12 @@
 import { ChakraProvider } from "@chakra-ui/react";
 import type { AppProps } from "next/app";
 
+import Navbar from "../components/Navbar";
+
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <ChakraProvider>
+      <Navbar />
       <Component {...pageProps} />
     </ChakraProvider>
   );
