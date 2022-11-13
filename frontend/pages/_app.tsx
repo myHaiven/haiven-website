65b96545920800f7c29d0ff19e6b9aa0727f316e
Jackie Lu <jlu6fz@gmail.com>
2022-11-13 10:01:53 -0800

    Add react to app scope
    I think nextjs already has react in scope, but I keep getting IDE errors
    so I'm just going to add it in anyways.

--- frontend/pages/_app.tsx
+++ frontend/pages/_app.tsx
@@ -1,5 +1,6 @@
 import { ChakraProvider } from "@chakra-ui/react";
 import type { AppProps } from "next/app";
+import React from "react";
 
 import Navbar from "../components/Navbar";
 import { customTheme } from "../styles/customTheme";
