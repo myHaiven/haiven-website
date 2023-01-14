162b98c66955b0903d4d7c9068858a8dae1cb4cf
Jackie Lu <46179386+jql6@users.noreply.github.com>
2023-01-13 21:42:18 -0800

    Redo "About page & refactoring" (#28)
    * Revert "Revert "About page & refactoring (#25)" (#27)"
    
    This reverts commit fa37ac65cea975a403cba3f8d85ba1ef661417f6.
    
    * Ignore out folder for eslint
    
    * Update text

--- frontend/src/pages/AppsPage.tsx
+++ frontend/src/pages/AppsPage.tsx
@@ -1,6 +1,16 @@
-import { Text } from "@chakra-ui/layout";
+import { Heading, Text } from "@chakra-ui/layout";
 import React from "react";
 
+import MainPanel from "../components/MainPanel";
+import { headingFontSizeBreakpoints } from "../styles/customTheme";
+
 export default function AppsPage() {
-  return <Text fontSize="5xl">Apps</Text>;
+  return (
+    <MainPanel>
+      <Heading fontSize={headingFontSizeBreakpoints} variant="primaryColor">
+        Apps
+      </Heading>
+      <Text variant="grey">Apps description here.</Text>
+    </MainPanel>
+  );
 }
