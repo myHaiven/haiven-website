08d9c2324338e8ac57a91ed89f6156d4ff66cbba
Jackie Lu <jlu6fz@gmail.com>
2023-01-13 22:39:08 -0800

    Add stats section

---
+++ frontend/sections/Stats.tsx
@@ -0,0 +1,27 @@
+import React from "react";
+import { Box, Heading, Text } from "@chakra-ui/react";
+
+export function Stats() {
+  return (
+    <Box>
+      <Box>
+        <Heading>In 2018...</Heading>
+        <Text>There were 68 000 transition home admissions in Canada</Text>
+      </Box>
+      <Box>
+        <Text>
+          107,810 police reported intimate partner violence (IPV) in Canada, yet
+          more than 1 in 10 men and women, approximately 2 million, self
+          reported having experienced IPV in the past 12 months.
+        </Text>
+      </Box>
+      <Box>
+        <Text>
+          At least 6.2 million people in Canada reported experiences of IPV in
+          their lifetime.
+        </Text>
+        <Text>Data collected from Statistics Canada</Text>
+      </Box>
+    </Box>
+  );
+}
