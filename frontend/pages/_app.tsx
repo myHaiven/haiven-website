1d92866be229356567c5bc2d0bc1c9b5fbb67d76
Jackie Lu <jlu6fz@gmail.com>
2023-01-13 23:01:18 -0800

    Add newsletter section

--- frontend/pages/_app.tsx
+++ frontend/pages/_app.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import { ChakraProvider } from "@chakra-ui/react";
 
 import Navbar from "../components/Navbar";
-import { Focus, Home, Stats, Values, Vision } from "../sections";
+import { Focus, Home, Newsletter, Stats, Values, Vision } from "../sections";
 import { customTheme } from "../styles/customTheme";
 
 export default function App() {
@@ -14,6 +14,7 @@ export default function App() {
       <Vision />
       <Focus />
       <Values />
+      <Newsletter />
     </ChakraProvider>
   );
 }
---
+++ frontend/sections/Newsletter.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+import { Box, Button, Heading } from "@chakra-ui/react";
+
+export function Newsletter() {
+  return (
+    <Box>
+      <Heading>{"Want to know what's next?"}</Heading>
+      <Button>Join our newsletter (WIP)</Button>
+    </Box>
+  );
+}
