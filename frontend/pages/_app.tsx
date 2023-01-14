1dda3dc3daa031f5b9090b20fc0c8d5ce47401c7
Jackie Lu <jlu6fz@gmail.com>
2023-01-13 22:27:10 -0800

    Remove unused app props

--- frontend/pages/_app.tsx
+++ frontend/pages/_app.tsx
@@ -1,12 +1,11 @@
 import React from "react";
 import { ChakraProvider } from "@chakra-ui/react";
-import type { AppProps } from "next/app";
 
 import Navbar from "../components/Navbar";
 import HomePanel from "../sections/HomePanel";
 import { customTheme } from "../styles/customTheme";
 
-export default function App({ Component, pageProps }: AppProps) {
+export default function App() {
   return (
     <ChakraProvider theme={customTheme}>
       <Navbar />
