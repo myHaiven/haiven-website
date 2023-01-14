d9259732ddd9051d9431999cd2ce8276c3dc5b7f
Jackie Lu <jlu6fz@gmail.com>
2023-01-13 22:29:23 -0800

    Rename home section

--- frontend/pages/_app.tsx
+++ frontend/pages/_app.tsx
@@ -2,14 +2,14 @@ import React from "react";
 import { ChakraProvider } from "@chakra-ui/react";
 
 import Navbar from "../components/Navbar";
-import HomePanel from "../sections/HomePanel";
+import Home from "../sections/Home";
 import { customTheme } from "../styles/customTheme";
 
 export default function App() {
   return (
     <ChakraProvider theme={customTheme}>
       <Navbar />
-      <HomePanel />
+      <Home />
     </ChakraProvider>
   );
 }
