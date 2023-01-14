816c39b68f14cf09a762352bbeae987951db46f2
Jackie Lu <jlu6fz@gmail.com>
2023-01-14 00:18:27 -0800

    Update layout for values

--- frontend/sections/Values.tsx
+++ frontend/sections/Values.tsx
@@ -1,28 +1,28 @@
 import React from "react";
-import { Box, Heading, Text } from "@chakra-ui/react";
+import { Box, Heading, Text, VStack } from "@chakra-ui/react";
 
 export function Values() {
   return (
-    <Box>
+    <VStack>
       <Heading>Our Values</Heading>
       <Box>
-        <Box>
+        <VStack>
           <Heading>Privacy</Heading>
           <Text>
             Out utmost priority to ensure their privacy and safety. We do so by
             consulting multiple partners, industry experts, and legal
             requirements.
           </Text>
-        </Box>
-        <Box>
+        </VStack>
+        <VStack>
           <Heading>Human-Centered</Heading>
           <Text>
             The person always comes first. Whether that is our user, our
             partners, or our team, we put the needs of the person before the
             needs of our organization.
           </Text>
-        </Box>
-        <Box>
+        </VStack>
+        <VStack>
           <Heading>Collaborative</Heading>
           <Text>
             The best work happens when we work together. We believe in
@@ -31,8 +31,8 @@ export function Values() {
             hear different perspectives, share different ideas, and work
             together to build products with meaning.
           </Text>
-        </Box>
-        <Box>
+        </VStack>
+        <VStack>
           <Heading>Freedom of Choice</Heading>
           <Text>
             We don’t always know what’s best for those we are supporting. Human
@@ -41,8 +41,8 @@ export function Values() {
             the freedom to make their own decisions and choose what’s best for
             them.
           </Text>
-        </Box>
+        </VStack>
       </Box>
-    </Box>
+    </VStack>
   );
 }
